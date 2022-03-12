212c4b5b1b7f5506c3f2046577e36d8654ef6b00
Martin <martinchuang1999@gmail.com>
2022-03-12 14:17:46 +0800

    Add get function

--- src/api/database.js
+++ src/api/database.js
@@ -18,3 +18,28 @@ async function getCities(db) {
   const cityList = citySnapshot.docs.map(doc => doc.data());
   return cityList;
 }
+
+async function getDocs(db, col) {
+  const queryCol = collection(db, col)
+  const querySnapshot = await getDocs(queryCol)
+  const queryList = querySnapshot.docs.map(doc => doc.data())
+  return queryList
+}
+
+//addCompany(db, doc),
+//where doc = {"id": "c1",
+        //     "data":{
+        //        "description":"Company 1 is a state-of-the-art tech firm that...",
+        //        "assignments":[
+        //            {"__datatype__":"documentReference","value":"assignments/c1a1"}],
+        //        "name":"Company 1",
+        //        "__collections__":{}
+        //        }
+        //     }
+async function addCompany(db, doc) {
+  await setDoc(doc(db, "companies", doc.id), doc.data)
+}
+
+async function addAssignment(db, doc) {
+  await setDoc(doc(db, "assignments", doc.id), doc.data)
+}
